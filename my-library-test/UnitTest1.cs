5871edf40b19b3a006a38af37e679aa55be96639
Thomas B Homburg <thomas@homburg.dk>
2024-04-15 15:23:12 +0200

    Add my-library class lib

---
+++ my-library-test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace MyLibrary.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
